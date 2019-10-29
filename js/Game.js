85cd81c6ada91aa164af1345a1c3b5820c01fb28
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-10-29 16:25:28 -0700

    first chunk.
    finished through step 6. tested to work, next step start step 7

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor(){
+    this.missed = 0;
+    this.phrases = [
+      {phrase: "say sike right now"},
+      {phrase: "stonks"},
+      {phrase: "flex tape"},
+      {phrase: "me and the boys"},
+      {phrase: "now this is an avengers level threat"}
+    ];
+    this.activePhrase = null;
+  }
+  getRandomPhrase(){
+  const randomNum = Math.floor(Math.random() * (5));
+  return game.phrases[randomNum];
+  }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+ class Phrase{
+   constructor(phrase){
+     this.phrase = phrase.toLowerCase();
+   }
+   addPhraseToDisplay(){
+    for (let i = 0; i < phrase.phrase.length; i++){
+      document.querySelector("#phrase").querySelector("ul").appendChild(document.createElement("li"));
+      document.querySelector("#phrase").querySelector("ul").childNodes[i].innerText = phrase.phrase[i];
+      if (document.querySelector("#phrase").querySelector("ul").childNodes[i].innerText !== ""){
+        document.querySelector("#phrase").querySelector("ul").childNodes[i].classList.add("hide", "letter", phrase.phrase[i]);
+    } else {
+      document.querySelector("#phrase").querySelector("ul").childNodes[i].innerText = " ";
+      document.querySelector("#phrase").querySelector("ul").childNodes[i].classList.add("space");
+    }
+    }
+  }
+};
